533137c859d20a613804af25e0245f79de6a972c
Mahera Furniturewala <maherafurniturewala1111@gmail.com>
2021-07-31 17:43:51 +0530

    setup server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const { log } = require('console');
+
+const publicPath = path.join(__dirname, '/../public');  //since the public folder will be the statics
+const port = process.env.PORT || 3000;
+var app = express();
+
+//build static folder
+app.use(express.static(publicPath));
+
+
+app.listen(port, ()=>{
+    console.log(`Server is up on port ${port}`);
+});
