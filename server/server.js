e77761d96536de64052c396727a08fde5d319691
Mahera Furniturewala <maherafurniturewala1111@gmail.com>
2021-07-31 18:11:20 +0530

    push to heroku

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const express = require('express');
-const { log } = require('console');
+
 
 const publicPath = path.join(__dirname, '/../public');  //since the public folder will be the statics
 const port = process.env.PORT || 3000;
