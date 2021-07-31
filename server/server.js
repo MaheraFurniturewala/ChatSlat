4b147f25d0686e55b79cfbe5ef1baa62764ccded
Mahera Furniturewala <maherafurniturewala1111@gmail.com>
2021-07-31 22:08:28 +0530

    add connect and disconnect event listeners

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
-
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '/../public');  //since the public folder will be the statics
 const port = process.env.PORT || 3000;
-var app = express();
+let app = express();
+//we did not have this instance in our express app that is why we needed to create our own server so we could pass it in socket.io
+let server = http.createServer(app);
+let io = socketIO(server);
 
 //build static folder
 app.use(express.static(publicPath));
 
+//this io(server instance) is going to listen to a pre-built event-->connection event
+//this socket(in the callback is the same that we have in the client side io method)
+io.on('connection',(socket) => {
+    console.log("A new user just connected");
+    socket.on('disconnect',() => {
+        console.log("User was disconnected");
+    });
+});
 
-app.listen(port, ()=>{
+
+server.listen(port, ()=>{
     console.log(`Server is up on port ${port}`);
 });
