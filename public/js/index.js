13d14cd1f55840ce4a715c4dc3da88bcdd79d04e
Mahera Furniturewala <maherafurniturewala1111@gmail.com>
2021-08-10 16:20:45 +0530

    adding messages to the body

--- public/js/index.js
+++ public/js/index.js
@@ -13,14 +13,26 @@
         
         socket.on('newMessage',function(message){
             console.log("New message",message);
+            let li = document.createElement('li');
+            li.innerText = `${message.from}:${message.text}`;
+
+            document.querySelector('body').appendChild(li);
         });
 // when we emit there are actually 3 args:
 // 1)The event name
 // 2)The data 
 // 3)The callback when the server gets it
-        socket.emit('createMessage',{
-            from:'John',
-            text:'Hey!!!'
-        }, function(message){
-            console.log("Server Got it   ",message);
-        });
+      
+// stopping the form from refreshing the page and grabbing values at the same time
+
+document.querySelector('#submit-btn').addEventListener('click',function(e){
+    e.preventDefault();
+
+    socket.emit('createMessage',{
+        from:'User',
+        text:document.querySelector('input[name="message"]').value,
+
+    },function(){
+
+    });
+});
