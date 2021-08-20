fe65e96740e9f29f3c5d4d40c88cd8f71efb02ca
Mahera Furniturewala <maherafurniturewala1111@gmail.com>
2021-08-20 18:15:14 +0530

    removed user

--- server/utils/users.js
+++ server/utils/users.js
@@ -1,18 +1,35 @@
 class Users {
-    constructor(name,age){
+    constructor() {
         //this refers to the instance(object) that is calling the class
         this.users = [];
     }
-    addUser(id,name,room){
-        let user = {
-            id,
-            name,
-            room
-        }
+    addUser(id, name, room) {
+        let user = { id, name, room }
         this.users.push(user);
         return user;
     }
 
+    getUserList(room) {
+        //filtering every user that has a room of user.room(here i will get the user object)
+        let users = this.users.filter((user) =>  user.room === room );
+        //names of those uses who are in that room
+        let namesArray = users.map((user) =>  user.name );
+        return namesArray;
+    }
+
+    getUser(id){
+        return this.users.filter((user)=> user.id === id)[0];
+    }
+
+    removeUser(id){
+        let user = this.getUser(id);
+          if(user){
+          this.users = this.users.filter((user) => user.id !== id);
+        }
+        return user;
+    }
+
+
 }
 
-module.exports = {Users};
+module.exports = { Users };
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -1,17 +1,77 @@
 const expect = require('expect');
 
-const {Users} = require('./users');
+const { Users } = require('./users');
 
-describe('Users',()=>{
+describe('Users', () => {
+    //creating c data
+    let users;
 
-    it('should add new user',()=>{
+    beforeEach(() => {
+        users = new Users();
+        users.users = [{
+            id: "1",
+            name: "John",
+            room: "Java",
+        },
+        {
+            id: "2",
+            name: "Pike",
+            room: "C++",
+        },
+        {
+            id: "3",
+            name: "Moses",
+            room: "Java",
+        }]
+    });
+
+    it('should add new user', () => {
         let users = new Users();
         let user = {
-            id:"abcd",
-            name:"Mahera",
-            room:"NodeJS",
+            id: "abcd",
+            name: "Milo",
+            room: "NodeJS",
         };
-        let resUser = users.addUser(user.id,user.name,user.room);
+        let reUser = users.addUser(user.id, user.name, user.room);
         expect(users.users).toEqual([user]);
-    })
-})
+    });
+
+    it('should return names for the Java room', () => {
+        let userList = users.getUserList('Java');
+
+        expect(userList).toEqual(['John', 'Moses']);
+    });
+
+    it('should return names for Scrubs Fans', () => {
+        let userList = users.getUserList('C++');
+
+        expect(userList).toEqual(['Pike']);
+    });
+
+    it('should find user', () => {
+        let userID = '2',
+            user = users.getUser(userID);
+        expect(user.id).toBe(userID);
+    });
+    it('should not find user', () => {
+        let userID = '12',
+            user = users.getUser(userID);
+        expect(user).toBeUndefined;
+    });
+    //to check id it is deleted from the array && it is returning user with the required id
+    it('should not remove a user', () => {
+        let userID = '108',
+            user = users.removeUser(userID);
+    
+        expect(user).toBeUndefined();
+        expect(users.users.length).toBe(3);
+      });
+    
+      it('should remove a user', () => {
+        let userID = '1',
+            user = users.removeUser(userID);
+    
+        expect(user.id).toBe(userID);
+        expect(users.users.length).toBe(2);
+      });
+});
